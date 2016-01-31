859d73b5d68046a9e72187527e9084c67129b27a
Chris <cpkarr@me.com>
2016-01-31 15:54:01 +0800

    Made threadcount a user entered variable. Tested with 20 threads. Some clean up

--- DiskConstrictorOO.py
+++ DiskConstrictorOO.py
@@ -9,9 +9,9 @@ import fcntl
 import random
 
 #---------------------- Runtime Configuaration Variables --------------------
-kDebugLevel         =   1
-kShowXferSpeeds     =   True
-kTestThreadCount    =   3
+kDebugLevel         =   0
+kShowXferSpeeds     =   False
+kMaxFiles           =   10000
 #----------------------------------------------------------------------------
 
 kOneMegabyte        =   1000000
@@ -20,19 +20,22 @@ kTotalUniqueChars   =   37    #there are 37 characters in alphabet plus 0-9 plus
 class IOTester:
 
     """ A single instance of a WRC tester
-    Uses up to 10 threads using a randomly generated multiplier which is the number of times to repeat a 37 unique character pattern:
-    from one up to a maximum of one million
+    Uses a randomly generated multiplier which is the number of times to repeat a 37 unique character pattern
     """
 
-    def __init__(self, multiple, instanceNumber):
-        self.charStrMultiple        =   multiple
-        self.sourceBuffer           =   bytes(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n' * multiple)
-        self.TotalBytes             =   kTotalUniqueChars * multiple
+    def __init__(self, instanceNumber):
+        if (instanceNumber == 0):
+            self.charStrMultiple    =   kOneMegabyte
+        elif (instanceNumber == 1):
+            self.charStrMultiple    =   1
+        else:
+            self.charStrMultiple        =   random.randrange(1,kOneMegabyte, 1)
+        self.sourceBuffer           =   bytes(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n' * self.charStrMultiple)
+        self.TotalBytes             =   kTotalUniqueChars * self.charStrMultiple
         self.megsXferred            =   self.TotalBytes / kOneMegabyte
         self.destBuffer             =   bytearray(self.TotalBytes)
         self.instanceNo             =   instanceNumber
-        if kDebugLevel  >   0:
-            print("\nSuccessfully initialized thread {0}".format(instanceNumber))
+        print("Successfully initialized thread {0}".format(instanceNumber + 1))
         return
 
     def WriteTestPattern(self):
@@ -44,30 +47,32 @@ class IOTester:
         global gOriginalDir
         xFerBytes    =   self.myFileH.readinto(self.destBuffer)
         if xFerBytes != self.TotalBytes:
-            print("Did not get the expected number of bytes")
-            gkeyboardinputstr = 'p'  #pause the test
+            print("Did not get the expected number of bytes. Pausing all tests.")
+            gkeyboardinputstr = 'p'  #pause all tests
         elif (self.sourceBuffer != self.destBuffer):
-            print("File Compare Error. Dumping Memory buffer into file 'MemBufferX' in script directory")
+            print("File Compare Error. Dumping Memory buffer into file 'MemBufferX' in script directory.\nPausing all tests")
+            gkeyboardinputstr = 'p'  #pause all tests
             try:
                 os.chdir(gOriginalDir)
-                dumpFile    =   open("MemBuffer{0}.bin".format(self.instanceNo))
+                dumpFile    =   open("MemBufferFor_" + self.testFileName + ".bin")
                 dumpFile.write(self.destBuffer)
                 dumpFile.close()
                 workingDirError      =   setTestWorkingDirectory()
             except:
                 print("Unexpected error:", sys.exc_info()[0])
-            gkeyboardinputstr = 'p'  #pause the test
         return
 
     def startNewTest(self):
-        print("\nCreating New Test File...\n")
-        self.testFileName    =   "testfile.bin"
-        for j in range(10000):
+        time.sleep((testThreadCount - self.instanceNo) // 2)    #give the thread creation proccess some time to spawn all the threads
+        if kDebugLevel > 0:
+            print("\nCreating New Test File...\n")
+        self.testFileName    =   "testfile1.bin"
+        for j in range(kMaxFiles):
             if (os.path.isfile(self.testFileName) == False):            #is there no file with this name already?
                 break                                               #yep, break out of loop
             else:
-                self.testFileName    =   "testfile{0}.bin".format(j+1)   #nope, try next file name
-        if (j == 9999):
+                self.testFileName    =   "testfile{0}.bin".format(j+2)   #nope, try next file name
+        if (j == kMaxFiles - 1):
             print("Sorry, exceeded the number of unique file names (10,000)\nExiting program. Try deleting all the test files in the 'TestFiles' directory")
             gkeyboardinputstr[0]    =   "q"     # exit the whole program
             exit(0)
@@ -109,14 +114,12 @@ class IOTester:
 def CheckForNewKeyboardInput():
     global gkeyboardinputstr
     if (gkeyboardinputstr == "q"):
-        print("\nExiting program")
-        return True
+        return True     # Exit out of this thread so program can terminate properly
     elif (gkeyboardinputstr == "p"):
         print("\nProgram Paused\nPress 'r' & <Return> to resume or 'q' & <Return> to quit")
         while (gkeyboardinputstr != "r"):
             if (gkeyboardinputstr == "q"):   # still allow user to quit from paused state
-                print("\nExiting program")
-                return True
+                return True     # Exit out of this thread so program can terminate properly
             time.sleep(1)   # slow down this loop so we don't consume all the CPU
         print("Resuming program...\n")
     return False
@@ -165,17 +168,16 @@ if (os.path.exists(gTestFilesFolder) == False):
 os.chdir(gTestFilesFolder)
 
 gkeyboardinputstr    =   "A"
-print("\nTo Start Press:  <s> <Enter>\nTo Pause Press:  <p> <Enter>\nTo Resume Press: <r> <Enter>\nTo Quit Press:   <q> <Enter>p")
+print("\nTo Start Press:  <s> <Enter>\nTo Pause Press:  <p> <Enter>\nTo Resume Press: <r> <Enter>\nTo Quit Press:   <q> <Enter>p\n")
+testThreadCount     =   eval(input("\nPlease entern the number of test threads you want to use: (You need a maximum of 37 MB of free memory per thread)"))
+
 kbThread            =  threading.Thread(target=getkeyboardinput_thread)
 kbThread.start()
 
-#testStrMultiple     =   random.randrange(1,kOneMegabyte, 1)
-testStrMultiple     =   1000000
-
-for i in range(kTestThreadCount):           # create the tester instances. The instances will spawn their own test threads
-    newTester   =   IOTester(testStrMultiple, i)
+for i in range(testThreadCount):           # create the tester instances. The instances will spawn their own test threads
+    newTester   =   IOTester(i)
     newTester.startNewTest()
 
-while gkeyboardinputstr != "q":
-    time.sleep(1)
-#   pass        #not sure if this takes a lot of CPU...
+print("\nStarting Test(s) running...\n")
+while (gkeyboardinputstr != "q" and gkeyboardinputstr != "p"):
+    time.sleep(1)       # Don't consume CPU for main event loop
